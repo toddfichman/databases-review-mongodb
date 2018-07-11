b8d52020ea4e92e1b738e76ccd5d6d0f38072d30
elaskowski <evalaskows@gmail.com>
2018-07-10 17:22:00 -0700

    Initial skeleton

---
+++ database-mongodb/bin/seedData.js
@@ -0,0 +1,28 @@
+const Promise = require('bluebird');
+Promise.promisifyAll(require('mongoose'));
+const db = require('../models');
+
+const listData = [
+  {
+    name: 'To Do List',
+  },
+];
+
+const todoData = [
+  {
+    name: 'Cook dinner',
+    list_name: 'To Do List'
+  },
+  {
+    name: 'Clean house',
+    list_name: 'To Do List'
+  },
+  {
+    name: 'Watch TV',
+    list_name: 'To Do List'
+  },
+  {
+    name: 'Sleep',
+    list_name: 'To Do List'
+  },
+];
